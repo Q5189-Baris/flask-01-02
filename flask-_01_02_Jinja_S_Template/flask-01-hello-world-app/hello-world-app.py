6974ec5e0ee6a30e300471f19d916355beae70b5
Baris <barissubasi0528@gmail.com>
2023-08-28 22:02:16 +0200

    flask push

---
+++ flask-_01_02_Jinja_S_Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,20 @@
+from flask import Flask 
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    return "hello chort 15"
+
+@app.route('/second')
+def second():
+    return 'this is second page'
+@app.route('/third')
+def third():
+    return 'this is third page'
+
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True, port=30000)
+    # app.run(host= '0.0.0.0', port=8081)
